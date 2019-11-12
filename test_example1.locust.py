9984098d32996c7ce070744823ca42479acf30e1
Guido Serra <zeph@fsfe.org>
2019-11-12 15:54:08 +0100

    so far so good, but only tavern validates the output

--- test_example1.locust.py
+++ test_example1.locust.py
@@ -2,26 +2,16 @@ from locust import HttpLocust, TaskSet, task
 
 class UserBehavior(TaskSet):
     def on_start(self):
-        """ on_start is called when a Locust start before any task is scheduled """
-        self.login()
-
-    def on_stop(self):
-        """ on_stop is called when the TaskSet is stopping """
-        self.logout()
-
-    def login(self):
-        self.client.post("/login", {"username":"ellen_key", "password":"education"})
-
-    def logout(self):
-        self.client.post("/logout", {"username":"ellen_key", "password":"education"})
-
-    @task(2)
-    def index(self):
+        """ ok, we do NOT have a session """
         self.client.get("/")
+        self.start_session()
+
+    def start_session(self):
+        self.client.put("/")
 
     @task(1)
-    def profile(self):
-        self.client.get("/profile")
+    def index(self):
+        self.client.get("/")
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
