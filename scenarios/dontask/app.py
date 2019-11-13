28005b11d04e25c2bbdef5cf703eca3946c81f6d
Guido Serra <zeph@fsfe.org>
2019-11-13 10:40:43 +0100

    sad reality

---
+++ scenarios/dontask/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, session, jsonify
+from flask_session import Session
+
+app = Flask(__name__)
+Session(app)
+
+@app.route('/', methods=["PUT"])
+def set():
+    session['key'] = 'value'
+    return jsonify("ok"), 201
+
+@app.route('/')
+def get():
+    value = session.get('key', 'not set')
+    http_code = 401 if value == 'not set' else 200
+    return jsonify({'value':value, 'count': 'Hello World! Forgive me...'}), http_code
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
