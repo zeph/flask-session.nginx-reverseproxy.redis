c797659dfd6e3aca6149758cb9ebe8e92671fc2f
Etienne Szott <etienne.szott@de.atu.eu>
2019-11-28 14:23:38 +0100

    hello dad

--- scenarios/dontask/src/main/java/sample/config/ServletConfig.java
+++ scenarios/dontask/src/main/java/sample/config/ServletConfig.java
@@ -1,9 +1,14 @@
 package sample.config;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import sample.CustomCookieSerializer;
+
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
 
 @Configuration
 public class ServletConfig {
@@ -11,5 +16,26 @@ public class ServletConfig {
     @Bean
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
+    }
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        CustomCookieSerializer serializer = new CustomCookieSerializer();
+        serializer.setCookieName("JSESSIONID");
+        serializer.setCookiePath("/");
+        serializer.setPostfix(getPostfix());
+        return serializer;
+    }
+
+    private String getPostfix() {
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            String ip = inetAddress.getHostAddress();
+            Integer workerId = Integer.parseInt(ip.substring(ip.lastIndexOf(".") + 1));
+            return ".worker" + workerId%4 ;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
